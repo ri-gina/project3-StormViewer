f91cd1d800a6775871f6cfb6a0a697a48528e998
Regina Mayorova <gina.rebrown@gmail.com>
2023-02-10 07:00:46 +0500

    commit

---
+++ StormViewer/DetailViewController.swift
@@ -0,0 +1,64 @@
+//
+//  DetailViewController.swift
+//  StormViewer
+//
+//  Created by Regina Mayorova on 04.02.2023.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+    var selectedImage: String?
+    var selectedPictureNumber = 0
+    var totalPictures = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Picture \(selectedPictureNumber) of \(totalPictures)"
+        navigationItem.largeTitleDisplayMode = .never
+        
+        //NEW adding right bar button
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+
+        if let imageToLoad = selectedImage {
+            imageView.image  = UIImage(named: imageToLoad)
+        }
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+    
+    //NEW sharing or saving
+   @objc func shareTapped() {
+       guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+           print("No image found")
+           return
+       }
+        let vc = UIActivityViewController(activityItems: [selectedImage!, image], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
